b0338d050051a35c259cb8f82a74fcf079a15430
GitHub Action <action@github.com>
2021-08-10 09:41:03 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/warmuuh/packageabbrevintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.warmuuh.packageabbrevintellijplugin.services
+
+import com.github.warmuuh.packageabbrevintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/warmuuh/packageabbrevintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/warmuuh/packageabbrevintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.warmuuh.packageabbrevintellijplugin.services
 
+import com.github.warmuuh.packageabbrevintellijplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
